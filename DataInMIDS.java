28abb8796bcf602c41436602e9e9dcfd0753fe59
Jeevitha Chandra <jeevithachandra22@gmail.com>
2016-05-19 12:09:19 -0400

    Data In MIDS: MIDS Algorithm

---
+++ DataInMIDS.java
@@ -0,0 +1,26 @@
+package multiple.instance.ds;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataInMIDS {
+	ArrayList<ArrayList<String>> pos = new ArrayList<ArrayList<String>>();
+	ArrayList<ArrayList<String>> neg = new ArrayList<ArrayList<String>>();
+	ArrayList<ArrayList<String>> DATA = new ArrayList<ArrayList<String>>();
+	ArrayList<String> RULEPOS = new ArrayList<String>();
+	ArrayList<String> RULENEG = new ArrayList<String>();
+	Map<String,String> RULE_BAGS = new HashMap<String,String>();
+	ArrayList<String> bagnames = new ArrayList<>();
+	ArrayList<Integer> bags_pos_neg = new ArrayList<Integer>();
+	String result="";
+	
+	int no_of_pos_bags=0;
+	int no_of_neg_bags=0;	
+	int no_of_bags = 0;
+	int[][] M;
+	int s=0;//number of instances
+	int k=0;//number of features
+	
+	
+}
